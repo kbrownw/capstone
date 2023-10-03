ab97d3e8f7ada8d54b069d6c0d31fe7e12ae350f
Keith Brown <kbrownw@gmail.com>
2023-10-03 14:28:45 -0500

    Add aria label to Button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Button = ( { url, buttonText }) => {
     return (
-        <Link to={ url } alt="Button">
+        <Link to={ url } alt="Button" aria-label="On click">
             <button className="btn-default">{ buttonText }</button>
         </Link>
     );
